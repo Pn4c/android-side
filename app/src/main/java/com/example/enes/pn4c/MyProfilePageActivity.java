adeb4ddee845232a45ecafd021ee8585129c2e77
Enescigdem <31999546+Enescigdem@users.noreply.github.com>
2017-09-19 11:39:04 -0700

    önemsiz değişiklikler

--- app/src/main/java/com/example/enes/pn4c/MyProfilePageActivity.java
+++ app/src/main/java/com/example/enes/pn4c/MyProfilePageActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 public class MyProfilePageActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile_page);
+
+
     }
 }
